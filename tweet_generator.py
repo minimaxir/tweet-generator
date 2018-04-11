a45234a78aa0ac650ed665b4e868df899ab9bdc4
Max Woolf <max@minimaxir.com>
2018-04-10 23:38:31 -0700

    Working version

--- tweet_generator.py
+++ tweet_generator.py
@@ -3,22 +3,29 @@
 import yaml
 import tweepy
 import re
-# from textgenrnn import textgenrnn
+from textgenrnn import textgenrnn
+
+
+def process_tweet_text(text):
+    text = re.sub(r'http\S+', '', text)   # Remove URLs
+    text = re.sub(r'@[a-zA-Z0-9_]+', '', text)  # Remove @ mentions
+    text = text.strip(" ")   # Remove whitespace resulting from above
+    text = re.sub(' +', ' ', text)   # Remove redundant spaces
+    return text
 
 
 with open("config.yaml", "r") as f:
     cfg = yaml.load(f)
-
-# textgen = textgenrnn()
-
 
 auth = tweepy.OAuthHandler(cfg['consumer_key'], cfg['consumer_secret'])
 auth.set_access_token(cfg['access_key'], cfg['access_secret'])
 
 api = tweepy.API(auth)
 
+texts = []
+context_labels = []
+
 for user in cfg['twitter_users']:
-    i = 0
     all_tweets = tweepy.Cursor(api.user_timeline,
                                screen_name=user,
                                count=200,
@@ -26,13 +33,12 @@ for user in cfg['twitter_users']:
                                include_rts=False).pages(16)
     for page in all_tweets:
         for tweet in page:
-            print(process_tweet_text(tweet.full_text))
-            i += 1
-            print(i)
+            tweet_text = process_tweet_text(tweet.full_text)
+            if tweet_text is not '':
+                texts.append(tweet_text)
+                context_labels.append(user)
 
-
-def process_tweet_text(text):
-    text = re.sub(r'http\S+', '', text)   # Remove URLs
-    text = re.sub(r'@[a-zA-Z0-9_]+', '', text)  # Remove @ mentions
-    text = text.strip(" ")   # Remove whitespace resulting from above
-    return text
+textgen = textgenrnn()
+textgen.train_on_texts(texts, context_labels, num_epochs=1)
+textgen.save('{}_twitter_weights.hdf5'.format("_".join(cfg['twitter_users'])))
+textgen.generate(20)
